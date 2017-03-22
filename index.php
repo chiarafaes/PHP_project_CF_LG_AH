310eebac027b8107b4278a28def37ce3cef3bc76
larissagoethals <goethals.larissa@hotmail.com>
2017-03-22 21:01:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-// signup & login-button 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Playlist</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
+
+    <link rel="stylesheet" href="css/style.css" />
+
+
+
+</head>
+
+<body>
+
+    <h1>Naam product</h1>
+    <h2>Hier komt slagzin</h2>
+
+    <form action="login.php" action="post" class="login">
+        <input name="login" type="submit" value="Login" />
+    </form>
+
+    <form action="registration.php" action="post" class="registration">
+        <input name="registration" type="submit" value="Join us" />
+    </form>
+
+
+</body>
+</html>
