3eaa91db88793b64f68837cad315d268694d3f8c
chiarafaes <chiarakefaes@hotmail.com>
2017-03-22 21:44:55 +0100

    include omgezet naar functie zodat er geen includefiles meer moeten geschreven worden

--- registration.php
+++ registration.php
@@ -1,5 +1,8 @@
 <?php
-    include_once ("classes/user.php");
+    spl_autoload_register(function ($class){
+    include_once ("classes/" . $class . ".class.php");
+    }); //vervangt includes
+
 
     if(!empty($_POST)){
         try{
