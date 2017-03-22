238c12d591c04de74c463acc0aac7552b58d089f
larissagoethals <goethals.larissa@hotmail.com>
2017-03-22 09:16:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,23 @@
     <title>Welcome to INSPIR8</title>
 </head>
 <body>
-    <form action="" id="login"></form>
+    <div class="box">
+        <form action="" method="post" id="login">
+        
+           <div class="form-group">
+                <label for="email">email </label>
+                <input name="email" id="email" type="email" />
+            </div>
+  
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input name="password" id="password" type="password" /> 
+            </div>
+  
+            <div class="form-group">
+                <button type="submit">Login</button>
+            </div>
+        </form> 
+    </div>
 </body>
 </html>
