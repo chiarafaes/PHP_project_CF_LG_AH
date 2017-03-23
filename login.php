8e13e3ffd7438c7c9480b7056265bedcd3362ce8
AlexHermans <alexhermans90@gmail.com>
2017-03-23 10:00:35 +0100

    Login created
    Login is aangemaakt, alsook een rudimentaire error handeling in de
    login.php page.
    Zie comments voor meer info.

--- login.php
+++ login.php
@@ -1,4 +1,53 @@
-<!DOCTYPE html>
+<?php
+    spl_autoload_register(function ($class){
+        include_once ("classes/".$class.".php");
+    });
+
+    if (!empty($_POST)){
+        try{
+            // gegevens opslaan
+            $user = new user();
+            $user->Mail = $_POST['email'];
+            $user->Password = $_POST['password'];
+
+            // query doen naar db om bcrypt wachtwoord terug te krijgen en te verifyen
+            $conn= Db::getInstance();
+            $statement = $conn->prepare("SELECT * FROM users WHERE Mail = :mail");
+            $statement->bindValue(":mail", $user->Mail);
+
+            if($statement->execute() && $statement->rowCount() != 0) {
+                $res = $statement->fetch(PDO::FETCH_ASSOC);
+
+                // hier gaan we het opgeslagen wachtwoord vergelijken met het ingegeven wachtwoord
+               if(password_verify($user->Password, $res['Password'])){
+                   // correct dus we starten de session
+                   session_start();
+
+                   // we maken session vars aan voor later
+                   $_SESSION['email'] = $user->Mail;
+                   $_SESSION['username'] = $res['Username'];
+                   $_SESSION['fullname'] = $res['Fullname'];
+
+                   // we sturen de user door
+                   header('location:loggedin.php');
+               } else{
+                   $error = 'Password does not match. Please try again';
+               }
+
+            } else {
+                $error = "User does not exist in database. Please register first.";
+            }
+
+            // Normaal gezien gaan we niet zeggen of het wachtwoord of de username fout is.
+            // We gaan gewoon zeggen dat indien één van de twee fout is dat de user niet kan inloggen.
+            // Dit is nu zo om te testen, maar in production kunnen/moeten we dat veranderen.
+
+        } catch (PDOException $e){
+            $error = $e->getMessage();
+        }
+    }
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +56,11 @@
 </head>
 <body>
     <div class="box">
+        <?php if (isset($error)):?>
+            <div class="error">
+                <?php echo $error; ?>
+            </div>
+        <?php endif; ?>
         <form action="" method="post" id="login">
         
            <div class="form-group">
