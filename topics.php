daa052a85b3b61d4c993890609e78b87686ca238
AlexHermans <alexhermans90@gmail.com>
2017-03-25 14:22:16 +0100

    Topics.php
    Nu de mogelijkheid om direct te zien welke topics gekozen zijn door jou
    en om deze aan te passen (rudimentair)

--- topics.php
+++ topics.php
@@ -1,6 +1,10 @@
 <?php
     session_start();
     $error = '';
+    $userTopics = [];
+    $insertTopics = [];
+    $mergedTopics = [];
+    $mode = '';
 
     spl_autoload_register(function ($class){
         include_once ("classes/".$class.".php");
@@ -9,15 +13,27 @@
     try {
         // enkel ingelogde users toegelaten!
         if (isset($_SESSION['email'])) {
-            echo "allez tis goed";
             $user = $_SESSION['email'];
 
             $conn = Db::getInstance();
 
             // alle topics inladen
-            // TODO: Query aanpassen naar inner join zodat topics van user kunnen worden vergeleken
             $statement = $conn->prepare("SELECT * FROM topics");
-            $statement->execute();
+            if($statement->execute()){
+
+                // nu dat de topics zijn ingeladen gaan we zien dewelke de user al had aangevinkt op voorhand
+                $userTopicsQuery = $conn->prepare("SELECT * FROM users_topics WHERE fk_users = :user ");
+                $userTopicsQuery->bindValue(':user', $user);
+
+                // We steken de resultaten in een array
+                if($userTopicsQuery->execute()){
+                    while($x = $userTopicsQuery->fetch(PDO::FETCH_ASSOC)){
+                        array_push($userTopics, $x['fk_topics']);
+                    }
+                }
+            } else {
+                $error = "There was a problem.";
+            }
 
             // Wanneer er gesubmit word, de inputs opslaan en queryen
             if(!empty($_POST)){
@@ -31,21 +47,55 @@
                     }
                 }
 
+                // Omdat we niet telkens opnieuw dezelfde topics willen inserten gaan we de twee arrays met topics
+                // met elkaar vergelijken en enkel de nieuwe topics onthouden. Als er een topic geen checkmark
+                // meer heeft moeten we deze verwijderen
+
+                if (count($topics) > count($userTopics)) {
+                    $insertTopics = array_diff($topics, $userTopics);
+                    $mode = 'add';
+                } elseif (count($topics) < count($userTopics)){
+                    $insertTopics = array_diff($userTopics, $topics);
+                    $mode = 'delete';
+                }
+
+
+
+                // we zien of er wel degelijk 5 topics gekozen zijn (XSS)
                 if (count($topics)>= 5) {
 
+                    //naargelang de mode gaan we de query aanpassen
+                    switch ($mode){
+                        case 'add':
+                            $query = $conn->prepare("INSERT INTO users_topics (fk_users, fk_topics) VALUES (:user, :topic)");
+                        break;
+                        case 'delete':
+                            $query = $conn->prepare("DELETE FROM users_topics WHERE fk_users = :user AND fk_topics = :topic");
+                    }
+
                     // we gaan per topic een query doen
-                    foreach ($topics as $topic) {
-                        $insert = $conn->prepare("INSERT INTO users_topics (fk_users, fk_topics) VALUES (:user, :topic)");
-                        $insert->bindValue(":user", $user);
-                        $insert->bindValue(":topic", $topic);
-                        if ($insert->execute()) {
+                    foreach ($insertTopics as $topic) {
+                        $query->bindValue(":user", $user);
+                        $query->bindValue(":topic", $topic);
+                        if ($query->execute()) {
                             $succes = "Your topics have been saved";
                         }
-
                     }
                 } else {
                     $error = "You need to pick at least 5 topics to continue.";
                 }
+            }
+
+            $mergedTopics = $userTopics;
+
+            // semi ajax ding
+            switch ($mode){
+                case 'add':
+                    $mergedTopics = array_merge($insertTopics, $userTopics);
+                break;
+                case 'delete':
+                    $mergedTopics = array_diff($userTopics, $insertTopics);
+                break;
             }
 
         } else {
@@ -54,6 +104,11 @@
     } catch (PDOException $e){
         $error = $e->getMessage();
     }
+var_dump($insertTopics);
+echo '<br>';
+var_dump($userTopics);
+echo '<br>';
+var_dump($mergedTopics);
 
 ?><!doctype html>
 <html lang="en">
@@ -77,7 +132,11 @@
         <fieldset>
             <?php while($res = $statement->fetch(PDO::FETCH_ASSOC)): ?>
                 <label for="<?php echo $res['name'];?>"><?php echo $res['name'];?></label>
-                <input class="topicInput" id="<?php echo $res['name'];?>" name="<?php echo $res['name'];?>" type="checkbox" value="<?php echo $res['id'];?>">
+                <input <?php foreach($mergedTopics as $key => $value){
+                    if ($value == $res['id']){
+                        echo 'checked';
+                    }
+                }?> class="topicInput" id="<?php echo $res['name'];?>" name="<?php echo $res['name'];?>" type="checkbox" value="<?php echo $res['id'];?>"><br>
             <?php endwhile; ?>
         </fieldset>
         <fieldset><button id="submit" disabled type="submit">Save topics</button></fieldset>
@@ -100,7 +159,7 @@
                     for (i = 0; i<topics.length; i++){
                         if (topics[i].checked){
                             ammountChecked += 1;
-                            if (ammountChecked == requiredAmmount){
+                            if (ammountChecked >= requiredAmmount){
                                 btn.disabled = false;
                             } else{
                                 btn.disabled = true;
