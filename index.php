83516a695ed2aad7521c8b53040a0981d21215eb
larissagoethals <goethals.larissa@hotmail.com>
2017-03-25 14:14:24 +0100

    Enkel terugkomende zaken worden hier opgemaakt (header, footer,...)

--- index.php
+++ index.php
@@ -5,21 +5,28 @@
     <title>Welcome - Inspir8</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
     <link rel="stylesheet" href="css/default.css" />
+
+    <link rel="stylesheet" href="css/index.css" />
+
 </head>
 
 <body>
 
-    <h1>IMD'terest</h1>
-    <h2>Hier komt slagzin</h2>
+   <div class="container"></div>
 
-    <form action="login.php" action="post" class="login">
-        <input name="login" type="submit" value="Login" />
-    </form>
+        <div class="logo">
+            <img src="img/logo.png" alt="logo"/>
+        </div>
 
-    <form action="registration.php" action="post" class="registration">
-        <input name="registration" type="submit" value="Join us" />
-    </form>
+        <h1>IMD'terest</h1>
+        <h2>Hier komt slagzin</h2>
 
+        <div class="buttons">
+            <a href="login.php" class="login">Login</a>
+            <a href="registration.php" class="registration">Join us</a>
+        </div>
+
+   </div>
 
 </body>
 </html>
