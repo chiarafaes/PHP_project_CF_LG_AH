67b3e40e0609e03ca2dc52f3eef54afc9a89e095
chiarafaes <chiarakefaes@hotmail.com>
2017-03-25 22:01:45 +0100

    homepage startup

--- home.php
+++ home.php
@@ -1,1 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Home</title>
+
+</head>
+<body>
+<div class="wrap">
+    <header>
+        <div class="logo">
+            <img src="img/logo.png" alt="logo"/>
+        </div>
+
+        <div class="search">
+            <form method="post" name="searching" action="#" id="searching" >
+                <input id="search" name="search" type="search" placeholder="search"/>
+                <button class="fa fa-search" aria-hidden="true" id="searchbutton" name="searchbutton" type="submit"></button>
+            </form>
+        </div>
+
+        <div class="iconen">
+            <div class="functions">
+                <div class="icon"> <a href="#" class="fa fa-th-large"></a> </div>
+                <div class="icon"> <a href="#" class="fa fa-comment"></a> </div>
+                <div class="icon"> <a href="#" class="fa fa-bell-o"></a> </div>
+            </div>
+            <div class="avatar">
+                <img src="https://s3.amazonaws.com/uifaces/faces/twitter/sillyleo/128.jpg">
+            </div>
+        </div>
+
+    </header>
+
+    <div class="sideBar">
+        <div class="sideBar__section">
+            <div class="sideBar__item is-side-bar-item-selected">Profile ></div>
+            <div class="sideBar__item">Privacy ></div>
+            <div class="sideBar__item">Conditions ></div>
+            <div class="sideBar__item">Contact ></div>
+            <div class="sideBar__item">Help ></div>
+        </div>
+        <div class="sideBar__section">
+            <div class="sideBar__item">Legal ></div>
+        </div>
+    </div>
+
+    <div class="right">
+
+    </div>
+</div>
+
+</body>
+</html>
