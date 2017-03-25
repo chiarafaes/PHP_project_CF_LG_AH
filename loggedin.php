c7a292e67f6a2315fc8e9121eb3b85d9ccd45e6f
chiarafaes <chiarakefaes@hotmail.com>
2017-03-25 20:54:21 +0100

    css aangepast

--- loggedin.php
+++ loggedin.php
@@ -17,30 +17,28 @@ else {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css" />
 
     <link rel="stylesheet" href="css/default.css" />
-
+    <link rel="stylesheet" href="css/loggedin.css" />
 
 
 </head>
 <body>
-<div class="box">
+    <div class="box">
 
-    <div class="avatar">
-        <img src="https://s3.amazonaws.com/uifaces/faces/twitter/sillyleo/128.jpg" alt="avatar" />
+        <div class="avatar">
+            <img src="https://s3.amazonaws.com/uifaces/faces/twitter/sillyleo/128.jpg" alt="avatar" />
+        </div>
+
+        <h1>Welcome back <?php echo $_SESSION['username']; ?></h1>
+        <p>Hier komt nog iets</p>
+
+            <form action="home.php" action="post" class="home" id="loggedin">
+                    <input name="continue" type="submit" value="Continue" />
+            </form>
+            <form action="logout.php" action="post" class="logout" id="loggedin">
+                    <input name="logout" type="submit" value="Logout of account" />
+             </form>
+
+
     </div>
-
-    <h1>Welcome back <?php echo $_SESSION['username']; ?></h1>
-    <p>Hier komt nog iets</p>
-
-
-    <form action="home.php" action="post" class="home">
-            <input name="continue" type="submit" value="Continue" />
-    </form>
-
-
-    <form action="logout.php" action="post" class="logout">
-            <input name="logout" type="submit" value="Logout of account" />
-     </form>
-
-</div>
 </body>
 </html>
