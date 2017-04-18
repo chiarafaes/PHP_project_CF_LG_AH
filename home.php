248c38171e4617d22a9296d4f05371c784c3a714
larissagoethals <goethals.larissa@hotmail.com>
2017-04-18 23:19:44 +0200

    masonry grid + aanpassingen homepage

--- home.php
+++ home.php
@@ -103,7 +103,7 @@ if(!empty($_POST['search'])){
                 <div class="img_holder">
                     <div class="buttons" id="1">
                         <a href="#" class="btn send">Send</a>
-                        <a href="#" class="btn save">Save</a>
+                        <a href="#" class="btn save">Save</a></br>
                         <a href="#" class="btn like">
                             <img src="img/<?php
                             if(!empty($likedPosts)) {
