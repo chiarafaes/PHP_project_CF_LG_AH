c10e979746b01c2211a6526a396fb0cabd932336
AlexHermans <alexhermans90@gmail.com>
2017-04-20 08:50:23 +0200

    Posts liked in AJAX tonen nu correct
    Items die opgehaald worden met AJAX kunnen nu geliked worden en
    onthouden dit in real time.

--- js/loadmore.js
+++ js/loadmore.js
@@ -5,39 +5,47 @@
 $(document).ready(function () {
     var offset = 0;
     $('#btn_loadmore').click(function (e) {
+        var likedPosts = [];
+        var isLiked = '';
         e.preventDefault();
-        offset += 5;
+        offset += 20;
+        $.ajax({
+            url: 'ajax/ajax.getlikedposts.php',
+            type: 'post',
+            success: function (data) {
+                likedPosts = $.merge(likedPosts, data);
+            }
+        })
         $.ajax({
             url: 'ajax/ajax.loadmore.php',
             type: 'post',
             data: {'offset': offset},
             success: function (data) {
-                var likedPosts = [];
-                $.ajax({
-                    url: 'ajax/ajax.getlikedposts.php',
-                    type: 'post',
-                    success: function (data) {
-
-                        likedPosts = data;
-                    }
-                })
                 console.log(likedPosts)
+                // console.log(likedPosts);
                 data.forEach(function (value) {
+                    $.each(likedPosts, function (index, likedValue) {
+                        if (value.id == likedValue.post){
+                            isLiked = '<img src="img/liked_icon.svg" />';
+                        } else {
+                            isLiked = '<img src="img/like_icon.svg" />';
+                        }
+                    })
                     var post = '<div class="pin" id="pinID-'+value.id+'">'+
                         '<div class="img_holder">'+
                         '<div class="buttons" id="1">'+
                         '<a href="#" class="btn send">Send</a>'+
                         '<a href="#" class="btn save">Save</a>'+
                         '<a href="#" class="btn like">'+
-                        '<img src="img/like_icon.svg" />'+
+                        isLiked+
                         '</a>'+
                         '</div>'+
                         '<a class="image ajax" href="#" title="photo 1" id="1">'+
                         '<img src="'+value.picture+'" alt="" >'+
                         '</a>'+
                         '</div>'+
-                        '<p class="description">'+value.description+'</p>'+
-                        '<p class="info"><span>0</span></p>'+
+                        '<p class="description">'+value.title+'</p>'+
+                        '<p class="likes"><span>0</span></p>'+
                         '<hr>'+
                         '<div class="user_info">'+
                         '<img src="#" alt="#">'+
@@ -47,6 +55,8 @@ $(document).ready(function () {
                         '</div>';
 
                     $('#left').append(post);
+
+                    console.log(isLiked)
                 })
             },
             error : function () {
