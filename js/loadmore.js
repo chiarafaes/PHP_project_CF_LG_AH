92f91474e6504092d6583f296e47ca5a7cfe42db
AlexHermans <alexhermans90@gmail.com>
2017-04-20 09:07:27 +0200

    Bugfix - loadmore.js
    Bug gefixed die ervoor zorgde dat ajax.loadmore.php voor
    ajax.getlikedposts gecalled werd wat zorgde voor problemen.

--- js/loadmore.js
+++ js/loadmore.js
@@ -14,54 +14,55 @@ $(document).ready(function () {
             type: 'post',
             success: function (data) {
                 likedPosts = $.merge(likedPosts, data);
-            }
-        })
-        $.ajax({
-            url: 'ajax/ajax.loadmore.php',
-            type: 'post',
-            data: {'offset': offset},
-            success: function (data) {
-                console.log(likedPosts)
-                // console.log(likedPosts);
-                data.forEach(function (value) {
-                    $.each(likedPosts, function (index, likedValue) {
-                        if (value.id == likedValue.post){
-                            isLiked = '<img src="img/liked_icon.svg" />';
-                        } else {
-                            isLiked = '<img src="img/like_icon.svg" />';
-                        }
-                    })
-                    var post = '<div class="pin" id="pinID-'+value.id+'">'+
-                        '<div class="img_holder">'+
-                        '<div class="buttons" id="1">'+
-                        '<a href="#" class="btn send">Send</a>'+
-                        '<a href="#" class="btn save">Save</a>'+
-                        '<a href="#" class="btn like">'+
-                        isLiked+
-                        '</a>'+
-                        '</div>'+
-                        '<a class="image ajax" href="#" title="photo 1" id="1">'+
-                        '<img src="'+value.picture+'" alt="" >'+
-                        '</a>'+
-                        '</div>'+
-                        '<p class="description">'+value.title+'</p>'+
-                        '<p class="likes"><span>0</span></p>'+
-                        '<hr>'+
-                        '<div class="user_info">'+
-                        '<img src="#" alt="#">'+
-                        '<p>'+value.username+'</p>'+
-                        '<p class="categorie">Categorie</p>'+
-                        '</div>'+
-                        '</div>';
+                $.ajax({
+                    url: 'ajax/ajax.loadmore.php',
+                    type: 'post',
+                    data: {'offset': offset},
+                    success: function (data) {
+                        console.log(likedPosts)
+                        // console.log(likedPosts);
+                        data.forEach(function (value) {
+                            $.each(likedPosts, function (index, likedValue) {
+                                if (value.id == likedValue.post){
+                                    isLiked = '<img src="img/liked_icon.svg" />';
+                                } else {
+                                    isLiked = '<img src="img/like_icon.svg" />';
+                                }
+                            })
+                            var post = '<div class="pin" id="pinID-'+value.id+'">'+
+                                '<div class="img_holder">'+
+                                '<div class="buttons" id="1">'+
+                                '<a href="#" class="btn send">Send</a>'+
+                                '<a href="#" class="btn save">Save</a>'+
+                                '<a href="#" class="btn like">'+
+                                isLiked+
+                                '</a>'+
+                                '</div>'+
+                                '<a class="image ajax" href="#" title="photo 1" id="1">'+
+                                '<img src="'+value.picture+'" alt="" >'+
+                                '</a>'+
+                                '</div>'+
+                                '<p class="description">'+value.title+'</p>'+
+                                '<p class="likes"><span>0</span></p>'+
+                                '<hr>'+
+                                '<div class="user_info">'+
+                                '<img src="#" alt="#">'+
+                                '<p>'+value.username+'</p>'+
+                                '<p class="categorie">Categorie</p>'+
+                                '</div>'+
+                                '</div>';
 
-                    $('#left').append(post);
+                            $('#left').append(post);
 
-                    console.log(isLiked)
+                            console.log(isLiked)
+                        })
+                    },
+                    error : function () {
+                        alert("oei niet goed");
+                    }
                 })
-            },
-            error : function () {
-                alert("oei niet goed");
             }
         })
+
     })
 });
