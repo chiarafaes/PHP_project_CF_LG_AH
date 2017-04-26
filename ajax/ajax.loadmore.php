f70cc6b1156f3b8f5df9a4c86e53eec457686b2f
AlexHermans <alexhermans90@gmail.com>
2017-04-26 22:25:23 +0200

    De uploadtijd is nu ook zichtbaar bij ajax-posts
    zie titel

--- ajax/ajax.loadmore.php
+++ ajax/ajax.loadmore.php
@@ -6,12 +6,12 @@
  * Time: 16:47
  */
 
-header("content-type:application/json");
+    header("content-type:application/json");
 
-spl_autoload_register(function ($class) {
-    include_once("../classes/".$class.".php");
-});
+    spl_autoload_register(function ($class) {
+        include_once("../classes/".$class.".php");
+    });
 
-$offset = $_POST['offset'];
+    $offset = $_POST['offset'];
 
-echo json_encode(Post::getPosts(20, $offset)); // moeten er 20 bijkomen (zoals in briefing)
+    echo json_encode(Post::getPosts(20, $offset)); // moeten er 20 bijkomen (zoals in briefing)
--- js/loadmore.js
+++ js/loadmore.js
@@ -2,6 +2,16 @@
  * Created by Alex on 17/04/2017.
  */
 
+// zelfde functie als Post::getTimeAgo() maar dan in jQuery
+function getTimeAgo(p_dDate)
+{
+    var currentDate = new Date();
+    var postDate = new Date(p_dDate);
+    var timeDiff = Math.abs(postDate.getTime() - currentDate.getTime());
+    var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+    return diffDays;
+}
+
 $(document).ready(function () {
     var offset = 0;
     $('#btn_loadmore').click(function (e) {
@@ -9,24 +19,30 @@ $(document).ready(function () {
         var isLiked = '';
         e.preventDefault();
         offset += 20;
-        $.ajax({
+        $.ajax(
+            {
             url: 'ajax/ajax.getlikedposts.php',
             type: 'post',
-            success: function (data) {
+            success: function (data)
+            {
                 likedPosts = $.merge(likedPosts, data);
-                $.ajax({
+                $.ajax(
+                    {
                     url: 'ajax/ajax.loadmore.php',
                     type: 'post',
                     data: {'offset': offset},
-                    success: function (data) {
-                        data.forEach(function (value) {
+                    success: function (data)
+                    {
+                        data.forEach(function (value)
+                        {
                             $.each(likedPosts, function (index, likedValue) {
                                 if (value.id == likedValue.post){
                                     isLiked = '<img src="img/liked_icon.svg" />';
                                 } else {
                                     isLiked = '<img src="img/like_icon.svg" />';
                                 }
-                            })
+                            });
+                            console.log();
                             var post = '<div class="pin" id="pinID-'+value.id+'">'+
                                 '<div class="img_holder">'+
                                 '<div class="buttons" id="1">'+
@@ -42,6 +58,7 @@ $(document).ready(function () {
                                 '</div>'+
                                 '<p class="description">'+value.title+'</p>'+
                                 '<p class="likes"><span>'+value.likes+'</span></p>'+
+                                '<p class="postdate"<span>'+getTimeAgo(value.postdate)+'d ago</span></p>'+
                                 '<hr>'+
                                 '<div class="user_info">'+
                                 '<img src="'+value.avatar+'" alt="#">'+
