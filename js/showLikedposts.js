904a36ad517b61a6fcb2053416124428564eb669
chiarafaes <chiarakefaes@hotmail.com>
2017-05-08 13:36:04 +0200

    liked posts worden weer gegeven -> nog css nodig

--- js/showLikedposts.js
+++ js/showLikedposts.js
@@ -18,6 +18,40 @@ $(document).ready(function () {
                 console.log(value);
                 container.html("");
 
+                var likedposts = value;
+
+                for (var prop in likedposts){
+                    if (likedposts[prop].private == 1){
+                        var check = "checked"
+                    } else {
+                        check = ""
+                    }
+                    var pin =
+                        '<div class="collection-container">'+
+                            '<h1 class="collection-title">'+likedposts[prop].title+'</h1>'+
+                        '</div>'+
+
+                        '<div class="posts-container">'+
+                            '<div class="pin"'+
+                                '<div class="img_holder">'+
+                                    '<img src="'+likedposts[prop].picture+'" >'+
+                                '</div>'+
+                                '<p class="description">'+likedposts[prop].description+' </p>'+
+                                '<p class="icon_heart">'+
+                                    '<img src="img/icon_hartjeLikes.svg">'+
+                                    '<p> '+likedposts[prop].likes+' </p>'+
+                                '</p>'+
+                                '<p> '+likedposts[prop].postdate+'</p>'+
+                                '<p> '+likedposts[prop].location+' </p>'+
+                                '<div>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>'+
+
+
+                    container.append(pin);
+                }
+
             },
             error: function () {
                 console.log('could not get Liked posts');
