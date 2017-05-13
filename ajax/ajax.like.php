6f9953710a0cac5c8ce169ed1f5ff7bb6d936b84
AlexHermans <alexhermans90@gmail.com>
2017-05-13 09:55:32 +0200

    BUGFIX: Likes trokken niet meer af
    Geen idee waarom

--- ajax/ajax.like.php
+++ ajax/ajax.like.php
@@ -35,7 +35,7 @@
             $delete->bindValue(':user', $user);
             $delete->bindValue(':post', $post);
 
-            $likes -= 0;
+            $likes -= 1;
 
             if ($delete->execute()) {
                 echo json_encode($likes);
